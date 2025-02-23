930ea388877e126267fd69521a943e8be31a6085
Hidden <64721048+Debug-Dan@users.noreply.github.com>
2025-02-23 02:23:52 -0500

    Initial alpha build

---
+++ EasyDolphinConfigurator/Form2.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace EasyDolphinConfigurator
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            searchBox = new TextBox();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(401, 139);
+            listBox1.TabIndex = 0;
+            // 
+            // searchBox
+            // 
+            searchBox.Location = new Point(12, 157);
+            searchBox.Name = "searchBox";
+            searchBox.Size = new Size(276, 23);
+            searchBox.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(searchBox);
+            Controls.Add(listBox1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form2";
+            Text = "Easy Dolphin Configurator";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox searchBox;
+    }
+}
---
+++ EasyDolphinConfigurator/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyDolphinConfigurator
+{
+    public partial class Form2 : Form
+    {
+        private List<string> allLines = new List<string>(); // Stores all lines from the file
+        private string selectedLine = ""; // Stores the selected line
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            LoadFile("C:\\Users\\danie\\Downloads\\wiitdb.txt"); // Replace with your actual file path
+        }
+        private void LoadFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                allLines = File.ReadAllLines(filePath).ToList();
+                listBox1.DataSource = new List<string>(allLines); // Efficiently binds the list
+            }
+            else
+            {
+                MessageBox.Show("File not found!");
+            }
+        }
+        private void searchBox_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = searchBox.Text.ToLower();
+            listBox1.DataSource = allLines.Where(line => line.ToLower().Contains(searchText)).ToList();
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                selectedLine = listBox1.SelectedItem.ToString();
+            }
+        }
+    }
+}
